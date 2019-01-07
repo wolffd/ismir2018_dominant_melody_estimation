7e44c2195c1468ed5f5dd1857bc91b200ba5cce0
wolffd <wolffd.mail@googlemail.com>
2019-01-07 15:49:25 +0000

    before multiprocessing, trying threaded approach for terminating stuck processes

--- predict/predict_on_single_audio_CRNN.py
+++ predict/predict_on_single_audio_CRNN.py
@@ -489,58 +489,9 @@ def main_prediction(file_path, dataset_name = 'medleydb', evaluate_results=False
 
 if __name__ == '__main__':
 
-    # a dataset name can be input as the first argument, e.g. "python predict_on_single_audio mysupercooldataset"
-    # this assumes that there is a folder "mysupercooldataset_audio" in ../../ from this file
-    if len(sys.argv) >= 2:
-        dataset = sys.argv[1]
-        rootDir = get_path_to_dataset_audio(dataset)
-        for dirName, subdirList, fileList in os.walk(rootDir):
-            # print('Found directory: %s' % (dirName))
-            
-            abbr_index = dirName.find(dataset + "_audio") + len(dataset + "_audio") + 1
-            if (abbr_index < len(dirName)):
-                abbreviated_dirname = dirName[abbr_index:]
-            else:
-                abbreviated_dirname = ''
-            # print('Processing: %s' % (abbreviated_dirname))
-             
-            # make all the new safe paths
-            if not os.path.exists(get_model_output_save_path(dataset) + '/' + abbreviated_dirname):
-                os.makedirs(get_model_output_save_path(dataset) + '/' + abbreviated_dirname)
-                
-            if not os.path.exists(get_hf0_path(dataset) + '/' + abbreviated_dirname):
-                os.makedirs(get_hf0_path(dataset) + '/' + abbreviated_dirname)                
-            
-            for fname in fileList:
-                if not ('.aiff') in fname.lower():
-                    continue
-                try:
-                    # print('\t%s' % fname)
-                    full_track_name = abbreviated_dirname + '/' + fname
-                    track_name = full_track_name[:-4]
-                    # track_name = 'test'
-                    
-                    # convert to wav if transformed
-                    transformed = False
-                    if not fname.lower().endswith('.wav'):
-                        conversion.convert_to_wav(rootDir + "/" + full_track_name)
-                        transformed = True
-                    
-                    HF0_fpath = '{0}/{1}.h5'.format(get_hf0_path(dataset),track_name)
-                    audio_fpath = '{0}/{1}.wav'.format(get_path_to_dataset_audio(dataset),track_name)
-                    print("Processing: %s" % audio_fpath)
-                    
-                    main_prediction(file_path=audio_fpath, dataset_name = dataset, evaluate_results=False)
-                    
-                    if transformed and not fname.lower().endswith('.wav'):
-                        os.remove(rootDir + "/" + full_track_name[:-4] + '.wav')
-                        
-                except Exception as e: 
-                    print ('Warning: could not process file %s: %s' % (full_track_name,str(e)))
-    else:
-        # Example usage:
-        track_name = 'AClassicEducation_NightOwl'
-        HF0_fpath = '{0}/{1}.h5'.format(get_hf0_path(),track_name)
-        audio_fpath = '{0}/{1}.wav'.format(get_path_to_dataset_audio(),track_name)
-        print(audio_fpath)
-        main_prediction(file_path=audio_fpath, evaluate_results=True)
+    # Example usage:
+    track_name = 'AClassicEducation_NightOwl'
+    HF0_fpath = '{0}/{1}.h5'.format(get_hf0_path(),track_name)
+    audio_fpath = '{0}/{1}.wav'.format(get_path_to_dataset_audio(),track_name)
+    print(audio_fpath)
+    main_prediction(file_path=audio_fpath, evaluate_results=True)
